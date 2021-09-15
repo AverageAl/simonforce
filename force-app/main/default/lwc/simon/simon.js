11ceb47ad259eb2b664dad1d1ad9790caf45b00f
unknown <al.waisley@gmail.com>
2021-09-15 12:21:48 -0500

    javascript update

--- force-app/main/default/lwc/simon/simon.js
+++ force-app/main/default/lwc/simon/simon.js
@@ -1,36 +1,68 @@
-import SystemModstamp from '@salesforce/schema/Account.SystemModstamp';
-import { lightning } from 'lwc';
+import { LightningElement } from 'lwc';
 
 export default class simon extends LightningElement {
-    let sequence = [];
-    let currentIndex = 0;
-    let userSelection;
-    let score = 0;
-    //
+    enum GAMETYPE {
+        CUSTOM,
+        STANDARD,
+    }
+    userSequence;
+    sequence = [];
+    currentIndex;
+    userSelection;
+    score;
+    gameStarted = false;
+    gameType = GAMETYPE.STANDARD;
     connectedCallback(){
-        sequence.push(Math.floor(Math.random() * 4));
+        this.sequence.push(Math.floor(Math.random() * 4));
+    }
+    // Take in user entered sequence and parse into list
+    handleUserSequence(event){
+        this.userSequence = event.detail.value;
+        this.userSequence = this.userSequence.split('').map(c => Number(c));
+        this.gameType = GAMETYPE.CUSTOM;
+    }
+    //Initialize Game
+    startGame(){
+        // ASK USER IF THEY WANT TO RESTART IF THEY ARE CURRENTLY IN GAME
+        this.gameStarted = true;
+        this.sequence = [];
+        score = 0;
+        this.currentIndex = 0;
+        getNext();
+    }
+    //End game
+    endGame(){
+        this.gameStarted = false;
+    }
+    getNext(){
+        if(this.gameType = GAMETYPE.CUSTOM){
+            this.sequence.push(this.userSequence[this.currentIndex]);
+        }
+        else{
+            this.sequence.push(Math.floor(Math.random() * 4));
+        }
+        this.currentIndex = 0;
     }
     checkUserSelection(){
-        //Correct answer
-        if (userSelection && sequence[currentIndex]){
-            currentIndex += 1;
-        }
-        //Incorrect answer. Showing final score and restarting game.
-        else {
-            //SHOW ALERT OF FINAL SCORE
-            score = 0;
-            sequence = [];
-            sequence.push(Math.floor(Math.random() * 4));
-            playSequence();
-            return;
-        }
-        //Finished sequence. Add next pattern to sequence.
-        if (currentIndex == sequence.length){
-            currentIndex = 0;
-            score += 1;
-            sequence.push(Math.floor(Math.random() * 4));
+        //Do nothing if the game is not started
+        if (this.gameStarted) {
+            //Correct answer
+            if (this.userSelection == this.sequence[currentIndex]){
+                this.currentIndex += 1;
+            }
+            //Incorrect answer. Showing final score and restarting game.
+            else {
+                //SHOW ALERT OF FINAL SCORE
+                endGame();
+                return;
+            }
+            if ((this.gameType == GAMETYPE.CUSTOM) && (this.currentIndex == this.userSequence.length)){
+                endGame();
+            }
+            //Finished sequence. Add next pattern to sequence.
+            if (this.currentIndex == this.sequence.length){
+                getNext();
+            }
         }
     }
-
-    playSequence(){}
 }
