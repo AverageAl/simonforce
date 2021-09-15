fad996786277452cb0e3e24674ad1ab2d3f24e91
Th3Pope <48426524+Th3Pope@users.noreply.github.com>
2021-09-15 10:32:28 -0700

    lynn frontend

--- force-app/main/default/lwc/simon/simon.js
+++ force-app/main/default/lwc/simon/simon.js
@@ -1,36 +1,36 @@
 import SystemModstamp from '@salesforce/schema/Account.SystemModstamp';
-import { lightning } from 'lwc';
+import { LightningElement } from 'lwc';
 
 export default class simon extends LightningElement {
-    let sequence = [];
-    let currentIndex = 0;
-    let userSelection;
-    let score = 0;
-    //
-    connectedCallback(){
-        sequence.push(Math.floor(Math.random() * 4));
-    }
-    checkUserSelection(){
-        //Correct answer
-        if (userSelection && sequence[currentIndex]){
-            currentIndex += 1;
-        }
-        //Incorrect answer. Showing final score and restarting game.
-        else {
-            //SHOW ALERT OF FINAL SCORE
-            score = 0;
-            sequence = [];
-            sequence.push(Math.floor(Math.random() * 4));
-            playSequence();
-            return;
-        }
-        //Finished sequence. Add next pattern to sequence.
-        if (currentIndex == sequence.length){
-            currentIndex = 0;
-            score += 1;
-            sequence.push(Math.floor(Math.random() * 4));
-        }
-    }
+    // let sequence = [];
+    // let currentIndex = 0;
+    // let userSelection;
+    // let score = 0;
+    // //
+    // connectedCallback(){
+    //     sequence.push(Math.floor(Math.random() * 4));
+    // }
+    // checkUserSelection(){
+    //     //Correct answer
+    //     if (userSelection && sequence[currentIndex]){
+    //         currentIndex += 1;
+    //     }
+    //     //Incorrect answer. Showing final score and restarting game.
+    //     else {
+    //         //SHOW ALERT OF FINAL SCORE
+    //         score = 0;
+    //         sequence = [];
+    //         sequence.push(Math.floor(Math.random() * 4));
+    //         playSequence();
+    //         return;
+    //     }
+    //     //Finished sequence. Add next pattern to sequence.
+    //     if (currentIndex == sequence.length){
+    //         currentIndex = 0;
+    //         score += 1;
+    //         sequence.push(Math.floor(Math.random() * 4));
+    //     }
+    // }
 
-    playSequence(){}
+    // playSequence(){}
 }
